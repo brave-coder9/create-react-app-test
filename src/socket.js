d01b990cff25fa4c779bd00c953ed5c2a49ba6a7
brave-coder9 <adala0814@gmail.com>
2024-11-25 07:15:23 +0100

    update

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,5 @@
 import { io } from "socket.io-client";
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL =
-  process.env.NODE_ENV === "production"
-    ? undefined
-    : "http://153.92.214.73:8080";
+const URL = process.env.REACT_APP_SOCKET_URL;
 
 export const socket = io(URL);
